f678b1798aece7abfac8031d5ff0623e687b2965
fqaiser94 <fqaiser94@gmail.com>
2017-12-16 14:17:58 -0500

    changed bash cmd for driving

--- drive.py
+++ drive.py
@@ -1,6 +1,6 @@
 # run using the following cmd
 # sudo python3 drive.py model.h5 /Desktop/run1
-# sudo python3 drive.py model_20171215_1946.h5 /Desktop/run1
+# sudo python3 drive.py model_20171215_2020.h5 /Desktop/run1
 
 
 import argparse
